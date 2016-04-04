b4d98dd1c2c2cbe00ca0ccbc43af8e652a38fe8a
elena05 <mariaelenamgs05@gmail.com>
2016-04-04 13:28:34 +0200

    ya lo tenemos

--- Calculadora/src/calculadora.java
+++ Calculadora/src/calculadora.java
@@ -3,12 +3,23 @@ public class calculadora {
 	//Atributos
 	private int n1;
 	private int n2;
-	//Constructores
+	private int result;
 	
+	//Constructores
 	public calculadora(int n1, int n2){
 		this.n1=n1;
 		this.n2=n2;
 	}
-	
-	
+	public int suma(){
+		return this.n1 + this.n2;
+	}
+	public int resta(){
+		return this.n1 - this.n2;
+	}
+	public int multiplicacion(){
+		return this.n1 * this.n2;
+	}
+	public double division(){
+		return (double)this.n1 / this.n2;
+	}
 }
